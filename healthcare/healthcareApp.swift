a1c82ff40fa0303468bfdf7b938e52d3babe8476
michaelSamir <en.michael.samir@gmail.com>
2022-07-26 17:36:44 +0200

    Initial Commit

---
+++ healthcare/healthcareApp.swift
@@ -0,0 +1,17 @@
+//
+//  healthcareApp.swift
+//  healthcare
+//
+//  Created by Michael Samir on 26/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct healthcareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
