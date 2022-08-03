f09c105e9f462ba8401551b000c32e7a5c3aac9c
michaelSamir <en.michael.samir@gmail.com>
2022-08-03 19:45:38 +0200

    first

---
+++ healthcare/OnBoarding.swift
@@ -0,0 +1,20 @@
+//
+//  OnBoarding.swift
+//  healthcare
+//
+//  Created by Michael Samir on 26/07/2022.
+//
+
+import SwiftUI
+
+struct OnBoarding: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OnBoarding_Previews: PreviewProvider {
+    static var previews: some View {
+        OnBoarding()
+    }
+}
